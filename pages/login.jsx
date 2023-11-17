ce9ef9d49ef88811ff174f52ce2690ee8ae25b4c
Rémi <rsagan@edulog.fr>
2023-11-17 12:34:55 +0100

    #feat > login page

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,3 +1,50 @@
+import {useState} from "react";
+import { useRouter } from 'next/router';
+
 export default () => {
-    return <>Bienvenu sur la page d'accueil</>
+    const router = useRouter();
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    });
+    const [isError, setIsError] = useState(false);
+
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            if (user.email === 'email' && user.password === 'password' ) {
+                await router.push('/about')
+            } else {
+                setIsError(true)
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    };
+
+
+    const handleChange = ({currentTarget}) => {
+        const {value, name} = currentTarget;
+        setUser(previousState => ({
+            ...previousState,
+            [name]: value
+        }));
+    }
+
+    return(
+        <>
+            <form onSubmit={handleSubmit}>
+                Identifiants :<br/>
+                E-mail : <b>email</b><br/>
+                Password : <b>password</b>
+                <hr/>
+                <input name="email" type="text" onChange={handleChange}/>
+                <input name="password" type="password" onChange={handleChange}/>
+                {isError && <p>Les identifiants sont incorrects</p>}
+                <button>Se connecter</button>
+            </form>
+        </>
+    );
 }
